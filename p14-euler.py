918d56e13a58dfcc2f1c2d7fec3d5b982f7ac186
chaege_Thinkpad <c.haege@ipc.uni-stuttgart.de>
2019-04-18 18:48:19 +0200

    Added p14-euler.py

---
+++ p14-euler.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 18 17:40:57 2019
+
+@author: Kischy
+"""
+
+
+print("Calculation started")
+
+p14_answ = 0
+
+
+print("The answer to the 14th problem of ProjectEuler.Net is:",p14_answ)
