35cabab6aee2d3c38c1870deec365ce5b842dc7b
Christian Kischy Haege <kischy@gmx.de>
2019-11-25 20:12:17 +0100

    Finished, brut force solution

--- collatz_sequence_length.py
+++ collatz_sequence_length.py
@@ -10,12 +10,35 @@ def is_even(number):
 
 def get_next_collatz_number(number):
     if(is_even(number)):
+        return number//2 
+    else:
+        return 3*number + 1          
+            
         
-
     
 def len_of_collatz_sequence(number):
-    length = 0
+    length = 1 
+    
     while(number != 1):
+        number = get_next_collatz_number(number)
+        length += 1
+        
+    return length
+
+
+def get_starting_number_with_longest_sequence(max_starting_num):
+    curr_length = 0
+    start_num = 0
+    
+    for i in range(1,max_starting_num):
+        tmp = len_of_collatz_sequence(i)
+        
+        if(tmp > curr_length):
+            curr_length = tmp
+            start_num = i       
+ 
+    return start_num
+    
         
         
     
--- p14-euler.py
+++ p14-euler.py
@@ -5,10 +5,14 @@ Created on Thu Apr 18 17:40:57 2019
 @author: Kischy
 """
 
+from collatz_sequence_length import get_starting_number_with_longest_sequence as long_collatz
+
+
 
 print("Calculation started")
 
-p14_answ = 0
+p14_answ = long_collatz(1000000)
+
 
 
 print("The answer to the 14th problem of ProjectEuler.Net is:",p14_answ)
